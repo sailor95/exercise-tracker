d38b72478cb0a68f074011c0afa82aade5f26fd8
buugman195 <bugman195@gmail.com>
2020-10-30 00:13:53 +0800

    Connect to backend

--- src/components/create-exercise/index.js
+++ src/components/create-exercise/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
@@ -13,9 +14,13 @@ export default class CreateExercise extends Component {
   };
 
   componentDidMount() {
-    this.setState({
-      users: ['test user'],
-      username: 'test user',
+    axios.get('http://localhost:5000/users/').then(res => {
+      if (res.data.length) {
+        this.setState({
+          users: res.data.map(user => user.username),
+          username: res.data[0].username,
+        });
+      }
     });
   }
 
@@ -57,6 +62,10 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
+    axios
+      .post('http://localhost:5000/exercises/add', exercise)
+      .then(res => console.log(res.data));
+
     window.location = '/';
   };
 
@@ -77,7 +86,9 @@ export default class CreateExercise extends Component {
               onChange={this.onChangeUsername}
             >
               {users.map(user => (
-                <option key={user} value={user}></option>
+                <option key={user} value={user}>
+                  {user}
+                </option>
               ))}
             </select>
           </div>
--- src/components/create-user/index.js
+++ src/components/create-user/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class EditUser extends Component {
   state = {
@@ -19,6 +20,10 @@ export default class EditUser extends Component {
     };
 
     console.log(user);
+
+    axios
+      .post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
 
     this.setState({
       username: '',
