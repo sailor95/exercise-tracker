c24684c12ce672a15a319c56880d0208f8d2b14d
David Huang <bugman195@gmail.com>
2020-10-29 22:22:34 +0800

    Setup frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+// import Navbar from './components/navbar';
+// import ExerciseList from './components/exercises-list';
+// import EditExercise from './components/edit-exercise';
+// import CreateExercise from './components/create-exercise';
+// import CreateUser from './components/create-user';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} /> */}
+    </Router>
   );
-}
+};
 
 export default App;
